c2b909bc6d61ece3eaa6e4c125c6424b02b11817
managementftradein-cyber <managementftradein@gmail.com>
2026-09-20 23:44:18 -0700

    Delete src directory

--- src/assets/extracted/index-script-2.js
+++
@@ -1,3 +0,0 @@
-
-(()=>{const o=document.createElement('div');o.className='tcc-transition';o.innerHTML='<span>TCC</span>';document.body.appendChild(o);
-document.querySelectorAll('a[href$=".html"]').forEach(a=>a.addEventListener('click',e=>{const h=a.getAttribute('href');if(!h||h.startsWith('http'))return;e.preventDefault();o.classList.add('active');setTimeout(()=>location.href=h,420)}));})();
--- src/tcc-config.js
+++
@@ -1,2 +0,0 @@
-
-window.TCC_CHURCH_ADDRESS = "";
